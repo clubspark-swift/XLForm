000a0a6c5ebbdcd4bfe6e192db33505037e829d4
Mike Pollard <mike.pollard@sportlabs.com>
2015-02-02 13:12:15 +0000

    Expose some methods publically so they can be overidden in a subclass

--- XLForm/XL/Cell/XLFormTextFieldCell.h
+++ XLForm/XL/Cell/XLFormTextFieldCell.h
@@ -32,4 +32,7 @@
 @property (nonatomic, readonly) UILabel * textLabel;
 @property (nonatomic, readonly) UITextField * textField;
 
+-(NSArray *)layoutConstraints;
+- (void)textFieldDidChange:(UITextField *)textField;
+
 @end
